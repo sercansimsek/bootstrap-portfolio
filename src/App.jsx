561fb8e77013a6d91df154650963684a223db48b
sercansimsek <sercansimsek44@gmail.com>
2026-06-29 07:29:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+      </header>
+    </div>
+  );
+};
