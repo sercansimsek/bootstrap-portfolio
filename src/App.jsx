60ea9bb2d585d3dcdbbca5dd6e3365470e1100bd
sercansimsek <sercansimsek44@gmail.com>
2026-06-29 14:06:44 +0200

    NavBar.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-// import { NavBar } from './components/NavBar';
+import { NavBar } from './components/NavBar';
 
 import './App.css';
-import { ResponsiveLayout } from './components/ResponsiveLayout';
+
 
 export const App = () => {
   return (
     <>
-      <ResponsiveLayout />
+      <NavBar />
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,51 +2,88 @@ import { useState } from 'react';
 import {
   Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
   NavLink,
+  NavItem,
   CloseButton,
+  NavbarToggler,
+  Button,
 } from 'reactstrap';
+import logo from '../assets/S-Logo.png';
+
+const navbarButton = {
+  color: '#34E0A1',
+  borderColor: '#34E0A1',
+  backgroundColor: '#34E0A1',
+  height: '40px',
+  width: '40px',
+  padding: 0,
+};
 
 export const NavBar = () => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
-
+  const toggleNavBar = () => setIsOpen(!isOpen);
   return (
-    <div>
+    <>
       <Navbar
-        color="faded"
-        className="d-flex justify-content-end navbar-expand-md navbar-light bg-light"
+        className="navbar-expand-md p-3"
+        style={{ backgroundColor: '#000' }}
       >
-        <NavbarBrand href="/" className="me-auto">
-          reactstrap
-        </NavbarBrand>
-        {!collapsed ? (
-          <CloseButton onClick={toggleNavbar} className="me-2" />
-        ) : (
-          <NavbarToggler onClick={toggleNavbar} className="me-2" />
-        )}
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <NavbarBrand href="#" className="me-3">
+            <img
+              src={logo}
+              alt="logo"
+              style={{ height: '50px', width: 'auto' }}
+            />
+          </NavbarBrand>
 
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+          <div className="d-flex align-items-center d-md-none">
+            {!isOpen ? (
+              <CloseButton
+                onClick={toggleNavBar}
+                className="me-2 d-flex align-items-center justify-content-center"
+                style={navbarButton}
+              />
+            ) : (
+              <NavbarToggler
+                onClick={toggleNavBar}
+                className="me-2 d-flex align-items-center justify-content-center"
+                style={navbarButton}
+              />
+            )}
+          </div>
+        </div>
+
+        <Collapse isOpen={!isOpen} navbar className="w-100">
+          <Nav navbar className="ms-auto align-items-md-center">
             <NavItem>
-              <NavLink href="#">Homepage</NavLink>
+              <NavLink href="#" style={{ color: '#34E0A1' }}>
+                Home
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">About</NavLink>
+              <NavLink href="#" style={{ color: '#34E0A1' }}>
+                About
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Projects</NavLink>
+              <NavLink href="#" style={{ color: '#34E0A1' }}>
+                Projects
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#">Contact</NavLink>
+              <Button className="btn btn-secondary btn-sm p-0">
+                <NavLink href="#" style={{ color: '#34E0A1' }}>
+                  Contact
+                </NavLink>
+              </Button>
             </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
-    </div>
+    </>
   );
 };
